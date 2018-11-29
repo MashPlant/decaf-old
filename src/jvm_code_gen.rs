7f4bd71f5b43cfa568f74ae5a1045d59230871fa
MashPlant <740678788@qq.com>
2018-11-30 06:54:57 +0800

    qs

--- src/jvm_code_gen.rs
+++ src/jvm_code_gen.rs
@@ -39,12 +39,12 @@ macro_rules! cmp {
   ($self_: expr, $cond: ident) => { {
     let before_else = $self_.new_label();
     let after_else = $self_.new_label();
-    $self_.method().$cond(before_else);
-    $self_.method().bool_const(false);
-    $self_.method().goto(after_else);
-    $self_.method().label(before_else);
-    $self_.method().bool_const(true);
-    $self_.method().label(after_else);
+    $self_.mb().$cond(before_else);
+    $self_.mb().bool_const(false);
+    $self_.mb().goto(after_else);
+    $self_.mb().label(before_else);
+    $self_.mb().bool_const(true);
+    $self_.mb().label(after_else);
   } };
 }
 
@@ -59,8 +59,6 @@ pub struct JvmCodeGen {
 
 trait ToJavaType {
   fn to_java(&self) -> JavaType;
-
-  fn to_java_with_dim(&self) -> (JavaType, u8);
 }
 
 // need to first judge whether it is string
@@ -78,16 +76,6 @@ impl ToJavaType for SemanticType {
       SemanticType::Object(class) => JavaType::Class(unsafe { (**class).name }),
       SemanticType::Array(elem) => JavaType::Array(Box::new(elem.to_java())),
       _ => unreachable!(),
-    }
-  }
-
-  fn to_java_with_dim(&self) -> (JavaType, u8) {
-    match self {
-      SemanticType::Array(elem) => {
-        let ret = elem.to_java_with_dim();
-        (ret.0, ret.1 + 1)
-      }
-      _ => (self.to_java(), 1)
     }
   }
 }
@@ -108,20 +96,26 @@ impl JvmCodeGen {
     self.program(&mut program);
   }
 
-  fn class(&self) -> &mut ClassBuilder {
+  fn cb(&self) -> &mut ClassBuilder {
     unsafe { &mut *self.class_builder }
   }
 
-  fn method(&self) -> &mut MethodBuilder {
+  fn mb(&self) -> &mut MethodBuilder {
     unsafe { &mut *self.method_builder }
   }
 
   fn store_to_stack(&self, t: &SemanticType, index: u8) {
-    handle!(t, self.method().i_store(index), self.method().a_store(index));
+    handle!(t, self.mb().i_store(index), self.mb().a_store(index));
   }
 
   fn load_from_stack(&self, t: &SemanticType, index: u8) {
-    handle!(t, self.method().i_load(index), self.method().a_load(index));
+    handle!(t, self.mb().i_load(index), self.mb().a_load(index));
+  }
+
+  fn new_local(&mut self) -> u8 {
+    let ret = self.stack_index;
+    self.stack_index += 1;
+    ret
   }
 
   fn new_label(&mut self) -> u16 {
@@ -176,7 +170,7 @@ impl Visitor for JvmCodeGen {
     let return_type = method_def.ret_t.to_java();
     // in type check, a virtual this is added to the param list
     // but jvm doesn't need it, so take the slice from 1 to end
-    let mut method_builder = MethodBuilder::new(self.class(),
+    let mut method_builder = MethodBuilder::new(self.cb(),
                                                 ACC_PUBLIC | if method_def.static_ { ACC_STATIC } else { 0 },
                                                 method_def.name,
                                                 &argument_types[if method_def.static_ { 0 } else { 1 }..],
@@ -219,7 +213,7 @@ impl Visitor for JvmCodeGen {
         var_def.index = self.stack_index;
         self.stack_index += 1;
       }
-      ScopeKind::Class(_) => self.class().define_field(ACC_PUBLIC, var_def.name, &var_def.type_.to_java()),
+      ScopeKind::Class(_) => self.cb().define_field(ACC_PUBLIC, var_def.name, &var_def.type_.to_java()),
       _ => unreachable!(),
     }
   }
@@ -232,12 +226,12 @@ impl Visitor for JvmCodeGen {
     let before_cond = self.new_label();
     let after_body = self.new_label();
     self.break_stack.push(after_body);
-    self.method().label(before_cond);
+    self.mb().label(before_cond);
     self.expr(&mut while_.cond);
-    self.method().if_eq(after_body);
+    self.mb().if_eq(after_body);
     self.block(&mut while_.body);
-    self.method().goto(before_cond);
-    self.method().label(after_body);
+    self.mb().goto(before_cond);
+    self.mb().label(after_body);
     self.break_stack.pop();
   }
 
@@ -246,13 +240,13 @@ impl Visitor for JvmCodeGen {
     let after_body = self.new_label();
     self.break_stack.push(after_body);
     self.simple(&mut for_.init);
-    self.method().label(before_cond);
+    self.mb().label(before_cond);
     self.expr(&mut for_.cond);
-    self.method().if_eq(after_body);
+    self.mb().if_eq(after_body);
     self.block(&mut for_.body);
     self.simple(&mut for_.update);
-    self.method().goto(before_cond);
-    self.method().label(after_body);
+    self.mb().goto(before_cond);
+    self.mb().label(after_body);
     self.break_stack.pop();
   }
 
@@ -260,31 +254,31 @@ impl Visitor for JvmCodeGen {
     let before_else = self.new_label();
     let after_else = self.new_label();
     self.expr(&mut if_.cond);
-    self.method().if_eq(before_else); // if_eq jump to before_else if stack_top == 0
+    self.mb().if_eq(before_else); // if_eq jump to before_else if stack_top == 0
     self.block(&mut if_.on_true);
-    self.method().goto(after_else);
-    self.method().label(before_else);
+    self.mb().goto(after_else);
+    self.mb().label(before_else);
     if let Some(on_false) = &mut if_.on_false { self.block(on_false); }
-    self.method().label(after_else);
+    self.mb().label(after_else);
   }
 
   fn break_(&mut self, _break: &mut Break) {
-    self.method().goto(*self.break_stack.last().unwrap());
+    self.mb().goto(*self.break_stack.last().unwrap());
   }
 
   fn return_(&mut self, return_: &mut Return) {
     if let Some(expr) = &mut return_.expr {
       self.expr(expr);
-      handle!(expr.get_type(), self.method().i_return(), self.method().a_return());
+      handle!(expr.get_type(), self.mb().i_return(), self.mb().a_return());
     } else {
-      self.method().return_();
+      self.mb().return_();
     }
   }
 
   fn new_class(&mut self, new_class: &mut NewClass) {
-    self.method().new_(new_class.name);
-    self.method().dup();
-    self.method().invoke_special(new_class.name, "<init>", &[], &JavaType::Void);
+    self.mb().new_(new_class.name);
+    self.mb().dup();
+    self.mb().invoke_special(new_class.name, "<init>", &[], &JavaType::Void);
   }
 
   fn new_array(&mut self, new_array: &mut NewArray) {
@@ -294,16 +288,16 @@ impl Visitor for JvmCodeGen {
       match &new_array.type_ {
         SemanticType::Array(elem_t) => match elem_t.as_ref() {
           SemanticType::Basic(name) => match *name {
-            "int" => self.method().new_int_array(),
-            "bool" => self.method().new_bool_array(),
-            "string" => self.method().a_new_array("java/lang/String"),
+            "int" => self.mb().new_int_array(),
+            "bool" => self.mb().new_bool_array(),
+            "string" => self.mb().a_new_array("java/lang/String"),
             _ => unreachable!(),
           }
           // I don't quite understand the design
           // class A[] => A
           // class A[][] => [[LA;
-          SemanticType::Object(class) => self.method().a_new_array((**class).name),
-          SemanticType::Array(_) => self.method().a_new_array(&elem_t.to_java().to_string()),
+          SemanticType::Object(class) => self.mb().a_new_array((**class).name),
+          SemanticType::Array(_) => self.mb().a_new_array(&elem_t.to_java().to_string()),
           _ => unreachable!(),
         }
         _ => unreachable!(),
@@ -326,13 +320,13 @@ impl Visitor for JvmCodeGen {
             let var_def = &*var_def;
             match (*var_def.scope).kind {
               ScopeKind::Local(_) | ScopeKind::Parameter(_) => self.store_to_stack(&var_def.type_, var_def.index),
-              ScopeKind::Class(class) => self.method().put_field((*class).name, var_def.name, &var_def.type_.to_java()),
+              ScopeKind::Class(class) => self.mb().put_field((*class).name, var_def.name, &var_def.type_.to_java()),
               _ => unreachable!(),
             }
           }
           Var::VarAssign(var_assign) => self.store_to_stack(&(*var_assign).type_, (*var_assign).index),
         }
-        Expr::Indexed(indexed) => handle!(&indexed.type_, self.method().i_a_store(), self.method().b_a_store(), self.method().a_a_store()),
+        Expr::Indexed(indexed) => handle!(&indexed.type_, self.mb().i_a_store(), self.mb().b_a_store(), self.mb().a_a_store()),
         _ => unreachable!(),
       }
     }
@@ -340,10 +334,10 @@ impl Visitor for JvmCodeGen {
 
   fn const_(&mut self, const_: &mut Const) {
     match const_ {
-      Const::IntConst(int_const) => self.method().int_const(int_const.value),
-      Const::BoolConst(bool_const) => self.method().bool_const(bool_const.value),
-      Const::StringConst(string_const) => self.method().string_const(&string_const.value),
-      Const::Null(_) => self.method().a_const_null(),
+      Const::IntConst(int_const) => self.mb().int_const(int_const.value),
+      Const::BoolConst(bool_const) => self.mb().bool_const(bool_const.value),
+      Const::StringConst(string_const) => self.mb().string_const(&string_const.value),
+      Const::Null(_) => self.mb().a_const_null(),
       _ => unimplemented!(),
     }
   }
@@ -352,18 +346,18 @@ impl Visitor for JvmCodeGen {
     match unary.op {
       Operator::Neg => {
         self.expr(&mut unary.r);
-        self.method().i_neg();
+        self.mb().i_neg();
       }
       Operator::Not => {
         let true_label = self.new_label();
         let out_label = self.new_label();
         self.expr(&mut unary.r);
-        self.method().if_eq(true_label);
-        self.method().bool_const(false);
-        self.method().goto(out_label);
-        self.method().label(true_label);
-        self.method().bool_const(true);
-        self.method().label(out_label);
+        self.mb().if_eq(true_label);
+        self.mb().bool_const(false);
+        self.mb().goto(out_label);
+        self.mb().label(true_label);
+        self.mb().bool_const(true);
+        self.mb().label(out_label);
       }
       _ => unreachable!()
     }
@@ -376,37 +370,37 @@ impl Visitor for JvmCodeGen {
         let out_label = self.new_label();
         let false_label = self.new_label();
         self.expr(&mut binary.l);
-        self.method().if_eq(false_label);
+        self.mb().if_eq(false_label);
         self.expr(&mut binary.r);
-        self.method().if_eq(false_label);
-        self.method().bool_const(true);
-        self.method().goto(out_label);
-        self.method().label(false_label);
-        self.method().bool_const(false);
-        self.method().label(out_label);
+        self.mb().if_eq(false_label);
+        self.mb().bool_const(true);
+        self.mb().goto(out_label);
+        self.mb().label(false_label);
+        self.mb().bool_const(false);
+        self.mb().label(out_label);
       }
       Or => {
         let out_label = self.new_label();
         let true_label = self.new_label();
         self.expr(&mut binary.l);
-        self.method().if_ne(true_label);
+        self.mb().if_ne(true_label);
         self.expr(&mut binary.r);
-        self.method().if_ne(true_label);
-        self.method().bool_const(false);
-        self.method().goto(out_label);
-        self.method().label(true_label);
-        self.method().bool_const(true);
-        self.method().label(out_label);
+        self.mb().if_ne(true_label);
+        self.mb().bool_const(false);
+        self.mb().goto(out_label);
+        self.mb().label(true_label);
+        self.mb().bool_const(true);
+        self.mb().label(out_label);
       }
       _ => {
         self.expr(&mut binary.l);
         self.expr(&mut binary.r);
         match binary.op {
-          Add => self.method().i_add(),
-          Sub => self.method().i_sub(),
-          Mul => self.method().i_mul(),
-          Div => self.method().i_div(),
-          Mod => self.method().i_rem(),
+          Add => self.mb().i_add(),
+          Sub => self.mb().i_sub(),
+          Mul => self.mb().i_mul(),
+          Div => self.mb().i_div(),
+          Mod => self.mb().i_rem(),
           Le => cmp!(self, if_i_cmp_le),
           Lt => cmp!(self, if_i_cmp_lt),
           Ge => cmp!(self, if_i_cmp_ge),
@@ -427,44 +421,44 @@ impl Visitor for JvmCodeGen {
 
   fn call(&mut self, call: &mut Call) {
     unsafe {
-      let method = &*call.method;
-      if !method.static_ {
+      let mb = &*call.mb;
+      if !mb.static_ {
         self.expr(if let Some(owner) = &mut call.owner { owner } else { unreachable!() });
       }
       if call.is_arr_len {
-        self.method().array_length();
+        self.mb().array_length();
         return;
       }
       for arg in &mut call.arg {
         self.expr(arg);
       }
-      let argument_types: Vec<JavaType> = method.param.iter().map(|var_def| var_def.type_.to_java()).collect();
-      let return_type = method.ret_t.to_java();
-      if method.static_ {
-        self.method().invoke_static((*method.class).name, method.name, &argument_types, &return_type);
+      let argument_types: Vec<JavaType> = mb.param.iter().map(|var_def| var_def.type_.to_java()).collect();
+      let return_type = mb.ret_t.to_java();
+      if mb.static_ {
+        self.mb().invoke_static((*mb.class).name, mb.name, &argument_types, &return_type);
       } else {
-        self.method().invoke_virtual((*method.class).name, method.name, &argument_types[1..], &return_type);
+        self.mb().invoke_virtual((*mb.class).name, mb.name, &argument_types[1..], &return_type);
       }
     }
   }
 
   fn print(&mut self, print: &mut Print) {
     for print in &mut print.print {
-      self.method().get_static("java/lang/System", "out", &JavaType::Class("java/io/PrintStream"));
+      self.mb().get_static("java/lang/System", "out", &JavaType::Class("java/io/PrintStream"));
       self.expr(print);
-      self.method().invoke_virtual("java/io/PrintStream", "print", &[print.get_type().to_java()], &JavaType::Void);
+      self.mb().invoke_virtual("java/io/PrintStream", "print", &[print.get_type().to_java()], &JavaType::Void);
     }
   }
 
   fn this(&mut self, _this: &mut This) {
-    self.method().a_load(0);
+    self.mb().a_load(0);
   }
 
   fn indexed(&mut self, indexed: &mut Indexed) {
     self.expr(&mut indexed.arr);
     self.expr(&mut indexed.idx);
     if !indexed.for_assign {
-      handle!(&indexed.type_, self.method().i_a_load(), self.method().b_a_load(), self.method().a_a_load());
+      handle!(&indexed.type_, self.mb().i_a_load(), self.mb().b_a_load(), self.mb().a_a_load());
     }
   }
 
@@ -478,7 +472,7 @@ impl Visitor for JvmCodeGen {
               ScopeKind::Local(_) | ScopeKind::Parameter(_) => self.load_from_stack(&var_def.type_, var_def.index),
               ScopeKind::Class(class) => {
                 self.expr(if let Some(owner) = &mut identifier.owner { owner } else { unreachable!() });
-                self.method().get_field((*class).name, var_def.name, &var_def.type_.to_java())
+                self.mb().get_field((*class).name, var_def.name, &var_def.type_.to_java())
               }
               _ => unreachable!(),
             }
